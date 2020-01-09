9ce8821226271af2b75c3c2ad7d5d86d6166d87f
edwardlay28 <edward@contensive>
2020-01-09 11:57:14 -0500

    Working on implementing the new version of spider docs

--- Spider/Spider/Models/Db/LinkAliasModel.vb
+++ Spider/Spider/Models/Db/LinkAliasModel.vb
@@ -1,11 +1,11 @@
 ﻿Imports Contensive.Models.Db
 
 Public Class LinkAliasModel
-    Inherits Contensive.Models.Db.DbBaseModel
+    Inherits DbBaseModel
 
     Public Shared ReadOnly Property tableMetadata As DbBaseTableMetadataModel = New DbBaseTableMetadataModel("Link Aliases", "ccLinkAliases")
 
-    Public Property pageId As Integer
+    Public Property pageid As Integer
     Public Property querystringsuffix As String
 
 End Class
--- Spider/Spider/SpiderClass.vb
+++ Spider/Spider/SpiderClass.vb
@@ -1,6 +1,8 @@
 ﻿Option Explicit On
+Imports System.IO
 Imports System.Net
 Imports Contensive.BaseClasses
+
 
 Public Class SpiderClass
     Inherits AddonBaseClass
@@ -8,13 +10,10 @@ Public Class SpiderClass
     Public Overrides Function Execute(ByVal CP As CPBaseClass) As Object
         Try
             Dim uptodate As Integer = 1
-
-            For Each link In Contensive.Models.Db.DbBaseModel.createList(Of LinkAliasModel)(CP)
-                CP.Site.ErrorReport("enterd for")
-
+            Dim links = Contensive.Models.Db.DbBaseModel.createList(Of LinkAliasModel)(CP)
+            For Each link In links
                 If (Not String.IsNullOrEmpty(link.name)) Then
-                    Dim pageid As Integer = link.pageId
-                    'might have to get by opening pagecont
+                    Dim pageid As Integer = link.pageid
                     Dim pagename As String = link.name.Replace("/", "")
                     Dim querystring As String = link.querystringsuffix
                     Dim host As String = CP.Site.DomainPrimary
@@ -23,29 +22,53 @@ Public Class SpiderClass
                     Dim linkName As String = link.name
                     Dim path As String = linkName
                     Dim pageNameLocation As Integer = linkName.IndexOf(pagename)
-                    If (pageNameLocation > 1) Then
-                        Dim linkSubstring = linkName.Substring(0, pageNameLocation)
-                        path = linkSubstring
-                    End If
+                    Dim linkSubstring = linkName.Substring(0, pageNameLocation)
+                    path = linkSubstring
                     Dim currentUrl As String = CP.Site.DomainPrimary + link.name
+                    Dim body As String = ""
+                    Dim finalUrl As String = ""
 
-                    Dim client As WebClient = New WebClient()
 
                     'download from https first
-                    ' currentUrl = "https://" + currentUrl
-                    Dim body As String = client.DownloadString(currentUrl)
+                    Dim httpsUrl As String = "https://" + currentUrl
+                    finalUrl = httpsUrl
+                    Dim httpsFailed As Boolean = False
+                    Try
+                        Dim uri As Uri = New Uri(httpsUrl)
+                        Dim request As HttpWebRequest = WebRequest.Create(uri)
+                        request.AutomaticDecompression = DecompressionMethods.GZip Or DecompressionMethods.Deflate
+                        Using response As HttpWebResponse = request.GetResponse()
+                            Using stream As Stream = response.GetResponseStream()
+                                Using reader As StreamReader = New StreamReader(stream)
+                                    body = reader.ReadToEnd()
+                                End Using
+                            End Using
+                        End Using
+                    Catch
+                        httpsFailed = True
+                    End Try
 
-                    '        Dim request As HttpWebRequest = WebRequest.Create(currentUrl) '(HttpWebRequest)
-                    '        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                    'Using (Dim response as HttpWebResponse = (HttpWebResponse)request.GetResponse())
-                    'Using (Stream stream = response.GetResponseStream())
-                    'Using (StreamReader reader = New StreamReader(stream)) {
-                    '     reader.ReadToEnd()
-
-                    '                End Using
-                    '            End Using
-                    '        End Using
-
+                    Dim httpFailed As Boolean = False
+                    If (httpsFailed) Then
+                        'download from http next
+                        Dim httpUrl = "http://" + currentUrl
+                        finalUrl = httpUrl
+                        Try
+                            Dim uri As Uri = New Uri(httpUrl)
+                            Dim request As HttpWebRequest = WebRequest.Create(uri)
+                            request.AutomaticDecompression = DecompressionMethods.GZip Or DecompressionMethods.Deflate
+                            Using response As HttpWebResponse = request.GetResponse()
+                                Using stream As Stream = response.GetResponseStream()
+                                    Using reader As StreamReader = New StreamReader(stream)
+                                        body = reader.ReadToEnd()
+                                    End Using
+                                End Using
+                            End Using
+                        Catch ex As Exception
+                            httpFailed = True
+                            CP.Site.ErrorReport(ex)
+                        End Try
+                    End If
 
                     'make a substring of the body from text search start to end
                     Dim substringStart As Integer = body.IndexOf("<!-- TextSearchStart -->")
@@ -54,13 +77,27 @@ Public Class SpiderClass
                     Dim body2 As String = body.Substring(substringStart, length)
                     Dim bodyText As String = CP.Utils.ConvertHTML2Text(body2)
 
-
                     'make a substring of the og:image
+                    Dim imageLink As String = ""
+                    Dim ogImageTag As String = "property=""og:image"" content="""
+                    Dim imageSubstringStart As Integer = body.IndexOf(ogImageTag)
+                    If (imageSubstringStart <> -1) Then
+                        'there is an og:image that can be used for the primary image link
+                        ' Dim ogimageTagEnd As Integer = ogImageTag.Length
+                        Dim primaryImageLinkStart As Integer = imageSubstringStart + ogImageTag.Length
+                        'ogImageTag + imageSubstringStart
+                        Dim imageLinkSub = body.Substring(primaryImageLinkStart)
+                        Dim finalImageSection As Integer = (imageLinkSub.IndexOf("""/>"))
+                        '+ primaryImageLinkStart
+                        'Dim finalImageSectionSize As Integer = finalImageSection - primaryImageLinkStart
+                        If finalImageSection > 0 Then
+                            imageLink = body.Substring(primaryImageLinkStart, finalImageSection)
+                        End If
+                    End If
 
-
-                    Dim cs As CPCSBaseClass = CP.CSNew()
+                        Dim cs As CPCSBaseClass = CP.CSNew()
                     'update the current record in spider docs
-                    If (cs.Open("Spider Docs", "link=" & CP.Db.EncodeSQLText(currentUrl))) Then
+                    If (cs.Open("Spider Docs", "name=" & CP.Db.EncodeSQLText(finalUrl))) Then
                         cs.SetField("host", host)
                         cs.SetField("path", path)
                         cs.SetField("uptodate", uptodate)
@@ -68,7 +105,11 @@ Public Class SpiderClass
                         cs.SetField("bodytext", bodyText)
                         cs.SetField("pageid", pageid)
                         cs.SetField("page", pagename)
-                        cs.SetField("name", currentUrl)
+                        cs.SetField("name", finalUrl)
+                        cs.SetField("link", currentUrl)
+                        If Not String.IsNullOrEmpty(imageLink) Then
+                            cs.SetField("primaryimagelink", imageLink)
+                        End If
                         cs.Save()
                         cs.Close()
                     Else
@@ -82,14 +123,18 @@ Public Class SpiderClass
                             cs.SetField("link", currentUrl)
                             cs.SetField("pageid", pageid)
                             cs.SetField("page", pagename)
-                            cs.SetField("name", currentUrl)
+                            cs.SetField("name", finalUrl)
+
+                            If Not String.IsNullOrEmpty(imageLink) Then
+                                cs.SetField("primaryimagelink", imageLink)
+                            End If
+
                             cs.Save()
                             cs.Close()
                         End If
                     End If
                 End If
             Next
-
         Catch ex As Exception
             CP.Site.ErrorReport(ex)
         End Try
