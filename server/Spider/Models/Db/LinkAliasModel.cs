3bcf5d23bef1194a3504c701f57241e4c038cef6
jkidwell <jay@contensive.com>
2026-05-01 20:59:42 -0400

    moved to analytics

--- server/Spider/Models/Db/LinkAliasModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Contensive.Addons.Spider {
-
-    public class LinkAliasModel : Contensive.Models.Db.LinkAliasModel {
-        public bool spidered { get; set; }
-        public DateTime datespidered { get; set; }
-    }
-}
